a2bdce44b3176d8e0518a147903df81b48732482
27k <mail@udesh.xyz>
2022-01-05 16:13:10 +0530

    Initial commit

---
+++ far.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Far
+# @raycast.mode compact
+#
+# Optional parameters:
+# @raycast.icon 🔭
+# @raycast.packageName Utility
+
+display_manager.py res 1536 960
+echo "That's bit far, eh!"
---
+++ near.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Near
+# @raycast.mode compact
+#
+# Optional parameters:
+# @raycast.icon 🔍
+# @raycast.packageName Utility
+
+display_manager.py res default
+echo "Looking near, eh!"
